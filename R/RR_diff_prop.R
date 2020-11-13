22cd0b548c2c714ca3bc517baed51251106c9d78
mwheymans <mwheymans@vumc.nl>
2020-11-13 08:35:03 +0100

    Implemented t-value.

--- R/RR_diff_prop.R
+++ R/RR_diff_prop.R
@@ -5,19 +5,23 @@
 #'
 #'@export
 RR_diff_prop <- function(est, se){
+  m <- length(est)
   mean_est <- mean(est)
   w_diff <-
     mean(se^2) # within variance
   b_diff <-
     var(est) # between variance
   tv_diff <-
-    w_diff + (1 + (1/length(est))) * b_diff # total variance
+    w_diff + (1 + (1/m)) * b_diff # total variance
   se_total <-
     sqrt(tv_diff)
+  r <- (1 + 1 / m) * (b_diff / w_diff)
+  v <- (m - 1) * (1 + (1/r))^2
+  t <- qt(0.975, v)
   ci_upper <-
-    mean_est + 1.96*se_total
+    mean_est + t*se_total
   ci_lower <-
-    mean_est - 1.96*se_total
+    mean_est - t*se_total
   res <-
     c(mean_est, se_total, ci_lower, ci_upper)
   return(res)
