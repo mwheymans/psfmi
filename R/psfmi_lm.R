943534f941bbccaf3aa56a398a3b2d05750a378d
mwheymans <mwheymans@vumc.nl>
2021-06-04 16:19:27 +0200

    New psfmi_lm versions for backward and forward selection of linear models

---
+++ R/psfmi_lm.R
@@ -0,0 +1,343 @@
+#' Pooling and Predictor selection function for backward or forward selection of
+#' Linear regression models in multiply imputed data.
+#'
+#' \code{psfmi_lm} Pooling and backward or forward selection of Linear regression
+#'  models in multiply imputed data using selection methods RR, D1, D2 and MPR.
+#'
+#' @param data Data frame with stacked multiple imputed datasets.
+#'   The original dataset that contains missing values must be excluded from the
+#'   dataset. The imputed datasets must be distinguished by an imputation variable,
+#'   specified under impvar, and starting by 1.
+#' @param formula A formula object to specify the model as normally used by glm.
+#'   See under "Details" and "Examples" how these can be specified. If a formula
+#'   object is used set predictors, cat.predictors, spline.predictors or int.predictors
+#'   at the default value of NULL.
+#' @param nimp A numerical scalar. Number of imputed datasets. Default is 5.
+#' @param impvar A character vector. Name of the variable that distinguishes the
+#' imputed datasets.
+#' @param Outcome Character vector containing the name of the continuous outcome variable.
+#' @param predictors Character vector with the names of the predictor variables.
+#'   At least one predictor variable has to be defined. Give predictors unique names
+#'   and do not use predictor name combinations with numbers as, age2, gender10, etc.
+#' @param cat.predictors A single string or a vector of strings to define the
+#' categorical variables. Default is NULL categorical predictors.
+#' @param spline.predictors A single string or a vector of strings to define the
+#' (restricted cubic) spline variables. Default is NULL spline predictors. See details.
+#' @param int.predictors A single string or a vector of strings with the names of the variables that form
+#'   an interaction pair, separated by a “:” symbol.
+#' @param keep.predictors A single string or a vector of strings including the variables that are forced
+#'   in the model during predictor selection. All type of variables are allowed.
+#' @param nknots A numerical vector that defines the number of knots for each spline predictor separately.
+#' @param p.crit A numerical scalar. P-value selection criterium. A value of 1
+#'   provides the pooled model without selection.
+#' @param method A character vector to indicate the pooling method for p-values to pool the
+#'   total model or used during predictor selection. This can be "RR", D1", "D2", "D3" or "MPR".
+#'   See details for more information. Default is "RR".
+#' @param direction The direction of predictor selection, "BW" means backward selection and "FW"
+#'   means forward selection.
+#'
+#' @details The basic pooling procedure to derive pooled coefficients, standard errors, 95
+#'  confidence intervals and p-values is Rubin's Rules (RR). However, RR is only possible when
+#'  the model included continuous or dichotomous variables. Specific procedures are
+#'  available when the model also included categorical (> 2 categories) or restricted cubic spline
+#'  variables. These pooling methods are: “D1” is pooling of the total covariance matrix,
+#'  ”D2” is pooling of Chi-square values and “MPR” is pooling of median p-values (MPR rule).
+#'  Spline regression coefficients are defined by using the rcs function for restricted cubic
+#'  splines of the rms package. A minimum number of 3 knots as defined under knots is required.
+#'
+#'  A typical formula object has the form \code{Outcome ~ terms}. Categorical variables has to
+#'  be defined as \code{Outcome ~ factor(variable)}, restricted cubic spline variables as
+#'  \code{Outcome ~ rcs(variable, 3)}. Interaction terms can be defined as
+#'  \code{Outcome ~ variable1*variable2} or \code{Outcome ~ variable1 + variable2 + variable1:variable2}.
+#'  All variables in the terms part have to be separated by a "+". If a formula
+#'  object is used set predictors, cat.predictors, spline.predictors or int.predictors
+#'  at the default value of NULL.
+#'
+#'@return An object of class \code{pmods} (multiply imputed models) from
+#'  which the following objects can be extracted: 
+#'  \itemize{
+#'  \item  \code{data} imputed datasets
+#'  \item  \code{RR_model} pooled model at each selection step
+#'  \item  \code{RR_model_final} final selected pooled model
+#'  \item  \code{multiparm} pooled p-values at each step according to pooling method
+#'  \item  \code{multiparm_final} pooled p-values at final step according to pooling method
+#'  \item  \code{multiparm_out} (only when direction = "FW") pooled p-values of removed predictors 
+#'  \item  \code{formula_step} formula object at each step
+#'  \item  \code{formula_final} formula object at final step
+#'  \item  \code{formula_initial} formula object at final step
+#'  \item  \code{predictors_in} predictors included at each selection step
+#'  \item  \code{predictors_out} predictors excluded at each step
+#'  \item  \code{impvar} name of variable used to distinguish imputed datasets
+#'  \item  \code{nimp} number of imputed datasets
+#'  \item  \code{Outcome} name of the outcome variable
+#'  \item  \code{method} selection method
+#'  \item  \code{p.crit} p-value selection criterium
+#'  \item  \code{call} function call
+#'  \item  \code{model_type} type of regression model used
+#'  \item  \code{direction} direction of predictor selection
+#'  \item  \code{predictors_final} names of predictors in final selection step
+#'  \item  \code{predictors_initial} names of predictors in start model
+#'  \item  \code{keep.predictors} names of predictors that were forced in the model    
+#' }
+#'
+#'
+#' @references Enders CK (2010). Applied missing data analysis. New York: The Guilford Press.
+#' @references van de Wiel MA, Berkhof J, van Wieringen WN. Testing the prediction error difference between
+#'   2 predictors. Biostatistics. 2009;10:550-60.
+#' @references Marshall A, Altman DG, Holder RL, Royston P. Combining estimates of interest in prognostic
+#'   modelling studies after multiple imputation: current practice and guidelines. BMC Med Res Methodol.
+#'   2009;9:57.
+#' @references Van Buuren S. (2018). Flexible Imputation of Missing Data. 2nd Edition. Chapman & Hall/CRC
+#'   Interdisciplinary Statistics. Boca Raton.
+#' @references EW. Steyerberg (2019). Clinical Prediction MOdels. A Practical Approach
+#'  to Development, Validation, and Updating (2nd edition). Springer Nature Switzerland AG.
+#'
+#' @references http://missingdatasolutions.rbind.io/
+#' 
+#' @author Martijn Heymans, 2021
+#'
+#' @examples
+#'   pool_lm <- psfmi_lm(data=lbpmilr, formula = Pain ~ factor(Satisfaction) + 
+#'   rcs(Tampascale,3) + Radiation + 
+#'   Radiation*factor(Satisfaction) + Age + Duration + BMI,
+#'   p.crit = 0.05, direction="FW", nimp=5, impvar="Impnr", 
+#'   keep.predictors = c("Radiation*factor(Satisfaction)", "Age"), method="D1")
+#'   
+#'   pool_lm$RR_model_final
+#' 
+#' @export
+psfmi_lm <- function(data,
+                     formula = NULL,
+                     nimp=5,
+                     impvar=NULL,
+                     Outcome = NULL,
+                     predictors=NULL,
+                     cat.predictors=NULL,
+                     spline.predictors=NULL,
+                     int.predictors=NULL,
+                     keep.predictors=NULL,
+                     nknots=NULL,
+                     p.crit=1,
+                     method="RR",
+                     direction=NULL)
+{
+  
+  call <- match.call()
+  
+  if(is_empty(formula)) {
+    if(is_empty(Outcome))
+      stop("Outcome variable not defined")
+   if(method=="D3")
+      stop("\n", "For linear regression use method D1, D2 or MPR", "\n") 
+    P <-
+      predictors
+    cat.P <-
+      cat.predictors
+    int.P <-
+      gsub(":", "*", int.predictors)
+    s.P <-
+      spline.predictors
+  } else{
+    form <-
+      terms(formula)
+    form_vars <-
+      attr(form, "term.labels")
+    if(is_empty(form_vars))
+      stop("\n", "No predictors defined, model is empty")
+    Outcome <-
+      as.character(attr(form, "variables")[[2]])
+    int.P <-
+      form_vars[grepl(paste(c("[*]", ":"), collapse = "|"), form_vars)]
+    int.P_temp <-
+      unique(unlist(str_split(int.P, paste(c("[*]", ":"), collapse = "|"))))
+    form_vars <-
+      form_vars[!grepl(paste(c("[*]", ":"), collapse = "|"), form_vars)]
+    form_vars <-
+      unique(c(form_vars, int.P_temp))
+    cat.P <-
+      form_vars[grepl("factor", form_vars)]
+    form_vars <-
+      form_vars[!grepl("factor", form_vars)]
+    s.P <-
+      form_vars[grepl("rcs", form_vars)]
+    nknots <-
+      c(readr::parse_number(s.P))
+    form_vars <-
+      form_vars[!grepl("rcs", form_vars)]
+    int.P <-
+      gsub(":", "*", clean_P(int.P))
+    cat.P <- clean_P(cat.P)
+    s.P <- clean_P(s.P)
+    P <- form_vars
+  }
+  keep.P <-
+    gsub(":", "*", keep.predictors)
+  
+  keep.P <-
+    sapply(as.list(keep.P), clean_P)
+  
+  P.check <-
+    c(P, cat.P, s.P)
+  # Check data input
+  if(p.crit!=1){
+    if(is_empty(direction))
+      stop("Specify FW or BW for forward or backward predictor selection")
+  }
+  if (!(is.data.frame(data)))
+    stop("Data should be a data frame")
+  data <- data.frame(as_tibble(data))
+  data <- mutate_if(data, is.factor, ~ as.numeric(as.character(.x)))
+  if(all(data[Outcome]==1 | data[Outcome]==0))
+    stop("Outcome should be a continuous variable")
+  if ((nvar <- ncol(data)) < 2)
+    stop("Data should contain at least two columns")
+  if(is_empty(impvar))
+    stop("Imputation variable is not defined")
+  if(is_empty(method)) method="RR"
+  if(all(!is_empty(cat.P) | !is_empty(s.P)) & method=="RR")
+    stop("Categorical or spline variables in model, define selection method: D1, D2 or MPR")
+  if (sort(unique(data[, impvar]))[1] == 0)
+    stop("Original dataset should not be included")
+  if(is_empty(nimp))
+    stop("Number of imputed datasets is not defined, use nimp!")
+  if (nimp < 2) {
+    stop("\n", "Number of imputed datasets must be > 1", "\n\n")
+  }
+  if (p.crit > 1)
+    stop("\n", "P-value criterium > 1", "\n")
+  if (any(nknots<3))
+    stop("\n", "Number of knots must be > 2", "\n")
+  if (length(nknots) != length(s.P))
+    stop("\n", "Number of knots not specified for every spline variable", "\n")
+  if (!is_empty(cat.P)) {
+    if(any(cat.P%in%P)){
+      cat.P.double <- cat.P[cat.P%in%P]
+      stop("\n", "Categorical variable(s) -", cat.P.double,
+           "- also defined as Predictor", "\n\n")
+    }
+  }
+  if (!is_empty(s.P)){
+    if(any(s.P%in%P)){
+      s.P.double <- s.P[s.P%in%P]
+      stop("\n", "Do not include Spline variable(s) -", s.P.double,
+           "- in predictors", "\n\n")
+    }
+  }
+  if(any(duplicated(P))){
+    stop("\n", "Predictor(s) - ", c(P[duplicated(P)]),
+         " - defined more than once", "\n\n")
+  }
+  # Check if al variables are available in dataset
+  if(any(!P.check %in% names(data))) {
+    P.mis <- P.check[!P.check %in% names(data)]
+    stop("\n", "Predictor(s) - ", P.mis,
+         "- not available in dataset", "\n\n")
+  }
+  if(!is_empty(int.P)) {
+    int.P.check <- lapply(int.P[grep("[*]", int.P)],
+                          function(x) { unlist(strsplit(x, split="[*]")) })
+    int.P.check <- unique(unlist(int.P.check))
+    if(any(!int.P.check %in% P.check))
+      stop("\n", "Not all interaction terms defined as
+        Predictor or Categorical Predictor", "\n\n")
+  }
+  # First predictors, second categorical
+  # predictors and last interactions
+  P <-
+    c(P, cat.P, s.P, int.P)
+  if (is_empty(P))
+    stop("\n", "No predictors to select, model is empty", "\n\n")
+  
+  if (!is_empty(keep.P)) {
+    for(i in 1:length(keep.P)){
+      if(grepl("[*]", keep.P[i])) {
+        keep.P.spl <- unlist(strsplit(keep.P[i], split="[*]"))
+        if(length(P[Reduce("&", lapply(keep.P.spl, grepl, P))])==0)
+          stop("Interaction term in keep.predictors not defined
+            as int.predictors, incorrect")
+        keep.P[i] <- P[Reduce("&", lapply(keep.P.spl, grepl, P))]
+      }
+    }
+  }
+  
+  if (!is_empty(cat.P)) {
+    if(length(cat.P)==1){
+      P <-
+        gsub(cat.P,
+             replacement=paste0("factor(", cat.P, ")"), P)
+      if(!is_empty(keep.P)){
+        keep.P <-
+          gsub(cat.P,
+               replacement=paste0("factor(", cat.P, ")"), keep.P)
+      }
+    } else {
+      for(i in 1:length(cat.P)) {
+        P <-
+          gsub(cat.P[i],
+               replacement=paste0("factor(", cat.P[i], ")"), P)
+        if(!is_empty(keep.P)){
+          keep.P <-
+            gsub(cat.P[i],
+                 replacement=paste0("factor(", cat.P[i], ")"), keep.P)
+        }
+      }
+    }
+  }
+  if (!is_empty(s.P)) {
+    if(length(s.P)==1){
+      P <-
+        gsub(s.P,
+             replacement=paste0("rcs(", s.P, ",", nknots, ")"), P)
+      if(!is_empty(keep.P)){
+        keep.P <-
+          gsub(s.P,
+               replacement=paste0("rcs(", s.P, ",", nknots, ")"), keep.P)
+      }
+    } else {
+      for(i in 1:length(s.P)) {
+        P <- gsub(s.P[i],
+                  replacement=paste0("rcs(", s.P[i], ",", nknots[i], ")"), P)
+        if(!is_empty(keep.P)){
+          keep.P <-
+            gsub(s.P[i],
+                 replacement=paste0("rcs(", s.P[i], ",", nknots[i], ")"), keep.P)
+        }
+      }
+    }
+  }
+  levels.cat.P <- lapply(cat.P, function(x) {
+    nr.levels.cat.P <- length(table(data[data[impvar] == 1, ][, x]))
+    if (nr.levels.cat.P < 3) {
+      stop("\n", "Categorical variable(s) only 2 levels,
+        do not define as categorical", "\n\n")
+    }
+  })
+  
+  if(any(!keep.P %in% P))
+    stop("\n", "Variables to keep not defined as Predictor", "\n\n")
+  
+  if(p.crit==1){
+    pobjpool <-
+      psfmi_lm_bw(data = data, nimp=nimp, impvar = impvar, Outcome= Outcome,
+                  P = P, p.crit = p.crit, method = method, keep.P = keep.P)
+    class(pobjpool) <-
+      "pmods"
+    return(pobjpool)
+  }
+  if(direction=="FW"){
+    pobjfw <-
+      psfmi_lm_fw(data = data, nimp = nimp, impvar = impvar, Outcome = Outcome, p.crit = p.crit,
+                  P = P, keep.P = keep.P, method = method)
+    class(pobjfw) <-
+      "pmods"
+    return(pobjfw)
+  }
+  if(direction=="BW"){
+    pobjbw <-
+      psfmi_lm_bw(data = data, nimp=nimp, impvar = impvar, Outcome= Outcome,
+                  P = P, p.crit = p.crit, method = method, keep.P = keep.P)
+    class(pobjbw) <-
+      "pmods"
+    return(pobjbw)
+  }
+}
---
+++ R/psfmi_lm_bw.R
@@ -0,0 +1,378 @@
+#' Backward selection of Linear regression models in multiply imputed data.
+#'
+#' \code{psfmi_lm_bw} Backward selection of Linear regression
+#' models in multiply imputed data using selection methods RR, D1, D2 and MPR.
+#' Function is called by \code{psfmi_lm}. 
+#'
+#' @param data Data frame with stacked multiple imputed datasets.
+#'   The original dataset that contains missing values must be excluded from the
+#'   dataset. The imputed datasets must be distinguished by an imputation variable,
+#'   specified under impvar, and starting by 1.
+#' @param nimp A numerical scalar. Number of imputed datasets. Default is 5.
+#' @param impvar A character vector. Name of the variable that distinguishes the
+#' imputed datasets.
+#' @param Outcome Character vector containing the name of the continuous outcome variable.
+#' @param P Character vector with the names of the predictor variables.
+#'   At least one predictor variable has to be defined. Give predictors unique names
+#'   and do not use predictor name combinations with numbers as, age2, BMI10, etc.
+#' @param p.crit A numerical scalar. P-value selection criterium. A value of 1
+#'   provides the pooled model without selection.
+#' @param method A character vector to indicate the pooling method for p-values to pool the
+#'   total model or used during predictor selection. This can be "RR", D1", "D2" or "MPR".
+#'   See details for more information. Default is "RR".
+#' @param keep.P A single string or a vector of strings including the variables that are forced
+#'   in the model during predictor selection. All type of variables are allowed.
+#'   
+#' @author Martijn Heymans, 2021
+#' @keywords internal
+#'  
+#' @export
+psfmi_lm_bw <- function(data, nimp, impvar, Outcome, P, p.crit, method, keep.P)
+{
+  call <- match.call()
+  
+  RR.model <- P_rm_step <- fm_step <- imp.dt <- multiparm <- list()
+  
+  P_orig <-
+    P
+  keep.P <-
+    sapply(as.list(keep.P), clean_P)
+  P_orig_temp <-
+    clean_P(P)
+  
+  if(!is_empty(keep.P))
+    if(length(P_orig)==1)
+      if(P_orig_temp == keep.P)
+        stop("\n", "No need to define keep.predictors. Exclude keep.predictors and set p.crit = 1","\n")
+  
+  # Loop k, to pool models in multiply imputed datasets
+  for (k in 1:(length(P)+1)) {
+    
+    # set regression formula fm
+    Y <-
+      c(paste(Outcome, paste("~")))
+    fm <-
+      as.formula(paste(Y, paste(P, collapse = "+")))
+    
+    # Extract df of freedom for MPR
+    if(method=="MPR" | method=="RR"){
+      
+      # Extract LR and P value of model for MPR
+      chi.LR <-
+        data.frame(matrix(0, length(attr(terms(fm), "term.labels")), nimp))
+      chi.p <-
+        data.frame(matrix(0, length(attr(terms(fm), "term.labels")), nimp))
+      
+      fit <- list()
+      for (i in 1:nimp) {
+        imp.dt[[i]] <- data[data[impvar] == i, ]
+        fit[[i]] <- glm(fm, data = imp.dt[[i]])
+        if(length(attr(terms(fm), "term.labels")) == 1){
+          chi.LR[, i] <- car::Anova(fit[[i]])[1]
+          chi.p[, i] <- car::Anova(fit[[i]])[3]
+        } else {
+          chi.LR[, i] <- car::Anova(fit[[i]])[, 1]
+          chi.p[, i] <- car::Anova(fit[[i]])[, 3]
+        }
+      }
+      
+      # Rubin's Rules
+      RR.model <- summary(pool(fit))
+      names(RR.model)[[k]] <-
+        paste("Step", k)#
+    }
+    
+    # D1 and D2 pooling methods
+    if(method=="D1" | method == "D2" ) {
+      
+      pool.p.val <-
+        matrix(0, length(P), 2)
+      P_test <-
+        clean_P(P)
+      
+      for (j in 1:length(P)) {
+        cov.nam0 <-
+          P[-j]
+        if (length(P) == 1) {
+          cov.nam0 <-
+            "1"
+        }
+        Y <-
+          c(paste(Outcome, paste("~")))
+        form1 <-
+          as.formula(paste(Y, paste(P, collapse = "+")))
+        form0 <-
+          as.formula(paste(Y, paste(cov.nam0, collapse = "+")))
+        
+        if(any(grepl(P_test[j], P_test[-j]))){
+          cov.nam0 <-
+            P[-grep(P_test[j], P_test)]
+          form0 <-
+            as.formula(paste(Y, paste(c(cov.nam0), collapse = "+")))
+        }
+        
+        fit1 <- fit0 <- imp.dt <- list()
+        for (i in 1:nimp) {
+          imp.dt[[i]] <-
+            data[data[impvar] == i, ]
+          fit1[[i]] <-
+            glm(form1, data = imp.dt[[i]])
+          fit0[[i]] <-
+            glm(form0, data = imp.dt[[i]])
+        }
+        RR.model[[k]] <-
+          summary(pool(fit1))
+        names(RR.model)[[k]] <-
+          paste("Step", k)
+        
+        test_P <-
+          mitml::testModels(fit1, fit0, method = method)
+        pvalue <-
+          test_P$test[4]
+        fstat <-
+          test_P$test[1]
+        pool.p.val[j, ] <-
+          c(pvalue, fstat)
+        
+      }
+      p.pool <-
+        data.frame(pool.p.val)
+      row.names(p.pool) <-
+        P
+      names(p.pool) <-
+        c(paste("p-values", method), "F-statistic")
+    }
+    
+    # MPR Pooling
+    if(method=="MPR") {
+      p.pool <-
+        data.frame(apply(chi.p, 1 , median))
+      rownames(p.pool) <-
+        P
+      names(p.pool) <-
+        c("p-value MPR")
+    }
+    
+    # RR Pooling
+    if(method=="RR") {
+      p.pool <-
+        data.frame(RR.model[[k]][-1, 6],
+                   row.names=P)
+      names(p.pool) <-
+        c("p-value RR")
+    }
+    
+    # Extract regression formula's
+    fm_step[[k]] <-
+      formula(fm)
+    names(fm_step)[[k]] <-
+      paste("Step", k)
+    # Extract multiparameter pooling
+    multiparm[[k]] <-
+      p.pool
+    names(multiparm)[[k]] <-
+      paste("Step", k)
+    
+    # Clean variable names for selection
+    P_temp <-
+      clean_P(row.names(p.pool))
+    p.pool_temp <-
+      p.pool
+    row.names(p.pool_temp) <-
+      P_temp
+    # detect interaction terms and exclude variables
+    # that are part of interaction
+    if(any(grepl("[*]", P))){
+      P_int_id <-
+        P_temp[grep("[*]", P_temp)]
+      # Detect main effects of interaction terms
+      P_main <-
+        unique(unlist(str_split(P_int_id, "[*]")))
+      # Exclude variables to keep and main effect from selection
+      remove_id <-
+        !row.names(p.pool_temp) %in% unique(c(P_main))
+      p.pool <-
+        p.pool[remove_id, , FALSE]
+      p.pool_temp <-
+        p.pool_temp[remove_id, , FALSE]
+    }
+    if(!is_empty(keep.P)){
+      remove_P_keep <-
+        !row.names(p.pool_temp) %in% keep.P
+      p.pool <-
+        p.pool[remove_P_keep, , FALSE]
+    }
+    
+    if(p.crit==1){
+      break()
+    }
+    
+    if(nrow(p.pool)==0)
+      break()
+    
+    # Select variables
+    P_temp <-
+      row.names(p.pool)
+    P_excl <-
+      which(p.pool[, 1] == max(p.pool[, 1]))
+    if(length(P_excl) > 1) {
+      P_excl <-
+        P_excl[1]
+    }
+    
+    if (p.pool[, 1][P_excl] < p.crit) {
+      message("\n", "Selection correctly terminated, ",
+              "\n", "No more variables removed from the model", "\n")
+      (break)()
+    }
+    
+    P_out <-
+      P_temp[P_excl]
+    
+    if(p.pool[, 1][P_excl] > p.crit) {
+      message("Removed at Step ", k,
+              " is - ", P_out)
+      
+    }
+    
+    P_rm_step[[k]] <-
+      P_out
+    # Variable excluded on each step
+    P <-
+      P[!P %in% P_out]
+    
+    if(is_empty(P)){
+      fm_step[[k+1]] <-
+        as.formula(paste(Y, 1))
+      names(fm_step)[[k+1]] <-
+        paste("Step", k+1)
+      multiparm[[k+1]] <-
+        0
+      names(multiparm)[[k+1]] <-
+        paste("Step", k+1)
+      fit <- list()
+      for (i in 1:nimp) {
+        imp.dt[[i]] <-
+          data[data[impvar] == i, ]
+        fit[[i]] <-
+          glm(fm_step[[k+1]], data = imp.dt[[i]])
+      }
+      
+      # Rubin's Rules
+      out.res <-
+        summary(pool(fit))
+      RR.model[[k+1]] <-
+        out.res
+      names(RR.model)[[k+1]] <-
+        paste("Step", k+1)
+      break()
+    }
+  }
+  # End k loop
+  ##############################################################
+  
+  P_rm_step_final <-
+    P_rm_step
+  if(is_empty(P)) {
+    P_rm_step <-
+      lapply(P_rm_step, clean_P)
+  } else {
+    P_rm_step <-
+      lapply(P_rm_step[-k], clean_P)
+  }
+  
+  # Extract selected models
+  outOrder_step <-
+    P_orig_temp
+  if(!is_empty(P_rm_step)){
+    P_remove <-
+      data.frame(do.call("rbind",
+                         lapply(P_rm_step, function(x) {
+                           outOrder_step %in% x })))
+    names(P_remove) <-
+      P_orig
+    row.names(P_remove) <-
+      paste("Step", 1:length(P_rm_step))
+    if(nrow(P_remove)!=1) {
+      P_remove <-
+        apply(P_remove, 2, function(x) ifelse(x, 1, 0))
+      P_remove_final <-
+        colSums(P_remove)
+      P_remove <-
+        rbind(P_remove, P_remove_final)
+      row.names(P_remove)[nrow(P_remove)] <-
+        "Removed"
+    } else {
+      P_remove <-
+        matrix(apply(P_remove, 2, function(x) ifelse(x, 1, 0)), 1, length(P_orig))
+      dimnames(P_remove) <-
+        list("Removed", P_orig)
+    }
+  } else {
+    P_remove <-
+      matrix(rep(0, length(P_orig)), 1, length(P_orig))
+    dimnames(P_remove) <-
+      list("Removed", P_orig)
+  }
+  
+  P_included <-
+    as_tibble(names(P_remove[nrow(P_remove), ][P_remove[nrow(P_remove), ] == 0] ))
+  predictors_final <-
+    names(P_remove[nrow(P_remove), ][P_remove[nrow(P_remove), ] == 0])
+  if(length(P_orig)==1 & !is_empty(P))
+    P_included <- predictors_final <- P
+  
+  if(is_empty(P) | p.crit==1){
+    RR_model_step <-
+      RR.model
+    multiparm_step <-
+      multiparm
+    fm_step_total <-
+      fm_step
+    if(p.crit!=1) names(RR_model_step) <- names(multiparm_step) <- names(fm_step_total) <-
+      paste("Step", 1:(k+1), "- removal -", c(unlist(P_rm_step_final), "ended"))
+    if(p.crit==1)  names(RR_model_step) <- names(multiparm_step) <- names(fm_step_total) <-
+      paste("Step", 1, "- no variables removed -")
+    RR_model_final <-
+      RR_model_step[k+1]
+    multiparm_final <-
+      multiparm_step[k+1]
+    fm_step_final <-
+      fm_step_total[k+1]
+  }
+  if(!is_empty(P) & p.crit !=1){
+    RR_model_step <-
+      RR.model
+    multiparm_step <-
+      multiparm
+    fm_step_total <-
+      fm_step
+    names(RR_model_step) <-
+      names(multiparm_step) <- names(fm_step_total) <-
+      paste("Step", 1:k, "- removal -", c(unlist(P_rm_step_final), "ended"))
+    RR_model_final <-
+      RR.model[k]
+    multiparm_final <-
+      multiparm[k]
+    fm_step_final <-
+      fm_step[k]
+  }
+  
+  Y_initial <-
+    c(paste(Outcome, paste("~")))
+  formula_initial <-
+    as.formula(paste(Y_initial, paste(P_orig, collapse = "+")))
+  
+  bw <-
+    list(data = data, RR_model = RR_model_step, RR_model_final = RR_model_final,
+         multiparm = multiparm_step, multiparm_final = multiparm_final,
+         formula_step = fm_step_total, formula_final = fm_step_final,
+         formula_initial = formula_initial,
+         predictors_in = P_included, predictors_out = P_remove,
+         impvar = impvar, nimp = nimp, Outcome = Outcome,
+         method = method, p.crit = p.crit, call = call,
+         model_type = "linear", direction = "BW",
+         predictors_final = predictors_final,
+         predictors_initial = P_orig, keep.predictors = keep.P)
+  return(bw)
+}
